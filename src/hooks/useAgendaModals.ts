9abfe6c5ba71d7ae9c5df069ead7e7ae0e6649fa
Matias Salinas <matisalinas1602@gmail.com>
2026-01-09 16:07:02 -0300

    hook_use_agenda_modals

---
+++ src/hooks/useAgendaModals.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+type ModalType = "create" | "edit" | "delete" | "view" | null;
+
+export default function useAgendaModals() {
+    const [modal, setModal] = useState<ModalType>(null);
+
+    const openCreate = () => {
+        setModal("create");
+    };
+
+    const openEdit = () => {
+        setModal("edit");
+    };
+
+    const openDelete = () => {
+        setModal("delete");
+    };
+
+    const openView = () => {
+        setModal("view");
+    };
+
+    const closeModal = () => {
+        setModal(null);
+    };
+    return {
+        modal,
+        openCreate,
+        openEdit,
+        openDelete,
+        openView,
+        closeModal,
+    }
+}
