336c2f8366d167bf589302ddadb3bc9365c9e5e9
Matias Salinas <matisalinas1602@gmail.com>
2026-01-09 16:06:05 -0300

    nota_refactored

--- src/components/Nota.tsx
+++ src/components/Nota.tsx
@@ -1,19 +1,25 @@
 import type { INote } from "../screens/agenda/agenda";
+import EditNote from "./modals/EditNote";
+import useAgendaModals from "../hooks/useAgendaModals";
 
-export default function Nota({ nota }: { nota: INote }) {
-    const handleEditNote = () => {
-
-    }
+export default function Nota({ note }: { note: INote }) {
+    const { modal, openEdit, closeModal } = useAgendaModals()
 
     return (
-        <li className="w-full p-4 mb-4 rounded-2xl text-white flex justify-between" style={{ backgroundColor: nota.color, color: nota.textColor }}>
-            <h2>{nota.title}</h2>
-            <p>{nota.title}</p>
-            <p>{nota.limitDate}</p>
+        <>
+        <li className="w-full p-4 mb-4 rounded-2xl text-white flex justify-between" style={{ backgroundColor: note.color, color: note.textColor }}>
+            <h2>{note.title}</h2>
+            <p>{note.title}</p>
+            <p>{note.limitDate}</p>
             <div className="w-2/10">
-                <button className="w-1/2 h-full bg-white/33 cursor-pointer hover:bg-white/66" onClick={handleEditNote}>E</button>
+                <button className="w-1/2 h-full bg-white/33 cursor-pointer hover:bg-white/66" onClick={openEdit}>E</button>
                 <button className="w-1/2 h-full bg-white/33 cursor-pointer hover:bg-red-500">X</button>
             </div>
         </li>
+        
+        {modal === "edit" &&
+            <EditNote note={note} closeModal={closeModal} />
+        }
+        </>
     )
 }
