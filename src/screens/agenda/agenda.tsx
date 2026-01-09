01c0453ce2abb3f1a2bb14efd362b4fa7fa56619
Matias Salinas <matisalinas1602@gmail.com>
2026-01-09 14:17:48 -0300

    state_notes_refactored_and_form_refactored

--- src/screens/agenda/agenda.tsx
+++ src/screens/agenda/agenda.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import FormularioAñadirNota from "../../components/FormularioAñadirNota";
+import { useEffect } from "react";
 import Nota from "../../components/Nota";
+import useNoteStore from "../../store/useNoteStore";
+import AñadirEditarNota from "../../components/AñadirEditarNota";
 
 export interface INote {
   id: number;
@@ -13,11 +14,40 @@ export interface INote {
   textColor: string;
 }
 
+export interface IFormValues {
+  title: string,
+  description: string,
+  assignature: string,
+  limitDate: string,
+  color: string
+}
+
 export const Agenda = () => {
-  const [notes, setNotes] = useState<INote[]>(() => {
-    const saved = localStorage.getItem("notes");
-    return saved ? JSON.parse(saved) : [];
-  });
+  const { notes, addNote, setNotes } = useNoteStore(state => state);
+  const initialValues: IFormValues = {
+    title: "",
+    description: "",
+    assignature: "",
+    limitDate: "",
+    color: ""
+  }
+
+  const handleCreate = (formValues: IFormValues) => {
+    const darkColors = ["#E54444", "#F38A48", "#56EB83", "#5F62F2", "#AF78EA", "#E171E6", "#535353"];
+
+    const newNote = {
+      id: notes.length + 1,
+      title: formValues.title,
+      description: formValues.description,
+      limitDate: formValues.limitDate,
+      completed: false,
+      assignature: formValues.assignature,
+      color: formValues.color,
+      textColor: darkColors.includes(formValues.color) ? "#ffffff" : "#222222",
+    }
+
+    addNote(newNote)
+  }
 
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
@@ -28,11 +58,11 @@ export const Agenda = () => {
       {notes.length === 0
         ? 'No hay agendas creadas'
         : <ul className="w-1/2">
-            { notes.map((nota, index) => (<Nota key={index + 1} nota={nota} />)) }
+            { notes.map((note, index) => (<Nota key={index + 1} note={note}/>)) }
         </ul>
       }
 
-      <FormularioAñadirNota notes={notes} setNotes={setNotes} />
+      <AñadirEditarNota initialValues={initialValues} onSubmit={handleCreate} />
     </div>
   )
 }
