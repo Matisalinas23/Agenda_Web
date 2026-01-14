d2ec5d1b5f341941d5614f7150ea3d6009aaafc5
Matias Salinas <matisalinas1602@gmail.com>
2026-01-14 12:24:18 -0300

    format_date_from_datetime_to_date

--- src/components/Nota.tsx
+++ src/components/Nota.tsx
@@ -6,12 +6,16 @@ import DeleteNote from "./modals/DeleteNote";
 export default function Nota({ note }: { note: INote }) {
     const { modal, openEdit, openDelete, closeModal } = useAgendaModals()
 
+    const formatDate = (date: string | Date) => {
+        return new Date(date).toLocaleDateString("es-AR")
+    }
+
     return (
         <>
         <li className="w-full p-4 mb-4 rounded-2xl text-white flex justify-between" style={{ backgroundColor: note.color, color: note.textColor }}>
             <h2>{note.assignature}</h2>
             <p>{note.title}</p>
-            <p>{note.limitDate}</p>
+            <p>{formatDate(note.limitDate)}</p>
             <div className="w-2/10">
                 <button className="w-1/2 h-full bg-white/33 cursor-pointer hover:bg-white/66" onClick={openEdit}>E</button>
                 <button className="w-1/2 h-full bg-white/33 cursor-pointer hover:bg-red-500" onClick={openDelete}>X</button>
