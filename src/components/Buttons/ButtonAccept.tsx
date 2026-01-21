1ea0cff61a6732fc99f8f61496407f33003cc943
Matias Salinas <matisalinas1602@gmail.com>
2026-01-21 18:50:15 -0300

    reubication_and_refactorization_buttons

---
+++ src/components/Buttons/ButtonAccept.tsx
@@ -0,0 +1,38 @@
+import "./buttonAccept.css"
+
+interface IAcceptButtonProps {
+    onClick?: () => void
+    text?: string
+    type: "button" | "reset" | "submit"
+}
+
+interface IAcceptButtonBase extends IAcceptButtonProps {
+    classExtension?: string
+    classname?: string
+}
+
+const ButtonAccept = ({ onClick, text, type, classname, classExtension }: IAcceptButtonBase) => (
+  <button onClick={onClick} type={type ?? "button"}
+    className={classname + " cursor-pointer py-2 bg-blue-500/80 text-neutral-100/90 font-bold rounded-full duration-200 " + classExtension}
+  >
+    <span className="relative">
+      {text ?? "ACEPTAR"}
+    </span>
+  </button>
+) // space at start and end off className is very important
+
+export const ButtonAcceptBig = ({ onClick, text, type }: IAcceptButtonProps) => (
+  <ButtonAccept onClick={onClick} text={text} type={type} classExtension="text-xl w-full" />
+)
+
+export const ButtonAcceptMedium = ({ onClick, text, type }: IAcceptButtonProps) => (
+  <ButtonAccept classExtension="text-lg w-48" onClick={onClick} type={type ?? "button"} text={text} />
+)
+
+export const ButtonAcceptSmall = ({ onClick, text, type }: IAcceptButtonProps) => (
+  <ButtonAccept classExtension="text-md w-24" onClick={onClick} type={type ?? "button"} text={text} />
+)
+
+export const ButtonRegister = ({ onClick, text, type }: IAcceptButtonProps) => (
+  <ButtonAccept classname="button-register" onClick={onClick} text={text} type={type} classExtension="relative overflow-hidden text-xl w-full" />
+)
---
+++ src/components/Buttons/ButtonCancel.tsx
@@ -0,0 +1,29 @@
+interface ICancelButtonProps {
+    onClick: () => void
+    text?: string
+}
+
+interface ICancelButtonBase extends ICancelButtonProps{
+  classname: string
+}
+
+const ButtonCancel = ({ onClick, text, classname }: ICancelButtonBase) => (
+  <button onClick={onClick}
+    className={"cursor-pointer py-2 bg-neutral-800 text-white rounded-full font-bold duration-200 hover:bg-neutral-600 " + classname}
+  >
+    {text ?? "ACEPTAR"}
+  </button>
+)
+
+export const ButtonCancelBig = ({ onClick, text }: ICancelButtonProps) => (
+  <ButtonCancel onClick={onClick} text={text} classname="text-xl w-full" />
+)
+
+export const ButtonCancelMedium = ({ onClick, text }: ICancelButtonProps) => (
+  <ButtonCancel onClick={onClick} text={text} classname="w-48" />
+)
+
+
+export const ButtonCancelSmall = ({ onClick, text }: ICancelButtonProps) => (
+  <ButtonCancel onClick={onClick} text={text} classname="text-sm w-24" />
+)
