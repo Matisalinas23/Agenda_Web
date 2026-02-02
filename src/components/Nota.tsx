bb259fa0ddfd09e9772e6504c78e84f610e488ba
Matias Salinas <matisalinas1602@gmail.com>
2026-02-02 15:56:02 -0300

    new delete and edit icons

--- src/components/Nota.tsx
+++ src/components/Nota.tsx
@@ -3,37 +3,47 @@ import EditNote from "./modals/EditNote";
 import useAgendaModals from "../hooks/useAgendaModals";
 import DeleteNote from "./modals/DeleteNote";
 import useDate from "../hooks/useDate";
-import { useState } from "react";
-import "./Note.css"
+import "./css/Note.css"
+import { EditIcon } from "./Icons/EditIcon";
+import { DeleteIcon } from "./Icons/DeleteIcons";
 
 export default function Nota({ note }: { note: INote }) {
-    const { modal, openEdit, openDelete, closeModal, openView } = useAgendaModals()
+    const { modal, openEdit, openDelete, closeModal } = useAgendaModals()
     const { formatDate } = useDate()
-    const [isOpen, setIsOpen] = useState<boolean>(false)
 
     return (
         <>
-        <li className={`note ${isOpen ? "open" : ""}`} style={{ backgroundColor: note.color, color: note.textColor }}>
-            <div className="w-full pb-4 flex justify-between">
-                <h2>{note.assignature}</h2>
-                <p>{note.title}</p>
-                <p>{formatDate(note.limitDate)}</p>
-                <div className="w-2/10">
-                    <button className="w-1/3 h-full bg-white/33 cursor-pointer hover:bg-neutral-500" onClick={openView}>V</button>
-                    <button className="w-1/3 h-full bg-white/33 cursor-pointer hover:bg-white/66" onClick={openEdit}>E</button>
-                    <button className="w-1/3 h-full bg-white/33 cursor-pointer hover:bg-red-500" onClick={openDelete}>X</button>
+        <li
+            className="w-full note font-medium"
+            style={{ backgroundColor: note.color, color: note.textColor }}
+        >
+            <div className="w-full flex gap-12">
+                <div className="w-full flex gap-12">
+                    <h2 className="min-w-44">{note.assignature}</h2>
+                    <p>{note.title}</p>
+                </div>
+
+                <div className="flex items-center gap-12">
+                    <p>{formatDate(note.limitDate)}</p>
+
+                    <div className="flex gap-4">
+                        <button className="w-6.5 h-6.5 rounded-lg cursor-pointer flex items-center justify-center" onClick={openEdit}>
+                            <EditIcon />
+                        </button>
+                        <button className="w-6.5 h-6.5 cursor-pointer" onClick={openDelete}>
+                            <DeleteIcon />
+                        </button>
+                    </div>
                 </div>
             </div>
 
             <div className="descriptionHidden">
-                <p className="bg-neutral-700/10 rounded-xl px-3 py-1">{note.description}</p>
+                <p className="bg-neutral-700/10 rounded-xl mt-4 px-3 py-1">{note.description}</p>
             </div>
-
-            <button className="w-full pb-2 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>v</button>
         </li>
-        
+
         { modal === "edit" && <EditNote note={note} closeModal={closeModal} /> }
-        { modal === "delete" && <DeleteNote note={note} closeModal={closeModal} />}
+        { modal === "delete" && <DeleteNote note={note} closeModal={closeModal} /> }
         </>
     )
 }
