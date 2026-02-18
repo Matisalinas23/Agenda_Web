4bee7dcbdc9707d8286d26bc2ad51288877f1998
Matias Salinas <matisalinas1602@gmail.com>
2026-02-18 14:49:01 -0300

    delete note modal have new style

--- src/components/modals/DeleteNote.tsx
+++ src/components/modals/DeleteNote.tsx
@@ -13,8 +13,8 @@ export default function DeleteNote({ note, closeModal }: { note: INote, closeMod
     }
 
     return (
-        <div className="w-full h-full fixed inset-0 z-10 bg-black/50 flex items-center justify-center">
-            <div className="p-8 bg-neutral-700 rounded-xl text-white flex flex-col justify-center items-center gap-10">
+        <div className="w-full h-full fixed inset-0 z-30 bg-black/50 flex items-center justify-center">
+            <div className="p-8 bg-neutral-100 shadow-lg rounded-3xl flex flex-col justify-center items-center gap-10">
                 <div className="text-center">
                     <p>Estas seguro que quieres eliminar de forma PERMANENTE la anotación:</p>
                     <div className="text-lg font-medium mt-2 flex gap-3 justify-center">
@@ -25,9 +25,11 @@ export default function DeleteNote({ note, closeModal }: { note: INote, closeMod
                 </div>
 
                 <div className="w-full flex justify-between">
-                    <ButtonCancelSmall onClick={closeModal} />
+                    <button className="bg-primary text-white font-medium px-2 rounded-full cursor-pointer duration-200 hover:bg-blue-400" onClick={closeModal}>
+                        CANCELAR
+                    </button>
                     <button
-                        className="w-24 py-1 bg-red-700 font-medium cursor-pointer rounded-xl duration-200 hover:bg-red-500"
+                        className="w-24 py-1 bg-red-500 text-white font-medium cursor-pointer rounded-full duration-200 hover:bg-red-400"
                         onClick={handleDeleteNote}
                     >
                         ELIMINAR
