1f85a290dbc854c41266eabe1050c5d1125591af
Matias Salinas <matisalinas1602@gmail.com>
2026-02-18 15:45:02 -0300

    navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,14 @@ import NavbarDeployable from "./NavbarDeployable";
 export default function Navbar() {
 
     return (
-        <nav className="text-white bg-neutral-500 flex">
+        <nav className="text-white shadow-lg font-normal bg-primary flex">
             <ul className='w-full py-4 px-8 flex gap-12' >
                 <li>
                     <Link to="/" > Home </Link>
                 </li>
             </ul>
 
-            <NavbarDeployable />
+            <NavbarDeployable /> {/* This is the user deployable. Deployable have Theme button, notifications button and close sesion button */}
         </nav>
     )
 }
--- src/components/NavbarDeployable.tsx
+++ src/components/NavbarDeployable.tsx
@@ -1,24 +1,44 @@
 import { useState } from "react"
 import { useAuth } from "../hooks/useAuth"
 import "./css/NavbarDeployable.css"
+import { RegisterLoginUserIcon } from "./Icons/RegisterLoginUserIcon"
+import { DarkModeIcon } from "./Icons/DarkModeIcon"
+import { NotificationsIcon } from "./Icons/NotificationsIcon"
+import { LogOutIcon } from "./Icons/LogOutIcon"
 
 export default function NavbarDeployable() {
     const { logoutUser } = useAuth()
     const [isOpenDepoyable, setIsOpenDepoyable] = useState<boolean>(false)
     return (
-        <div className={`navbarSideSection ${isOpenDepoyable ? "open" : ""}`}>
-                <button
-                        className="h-14 cursor-pointer"
-                        onClick={() => setIsOpenDepoyable(!isOpenDepoyable)}
-                    >
-                        <div className="bg-neutral-800 w-10 h-10 rounded-full"></div>
-                </button>
-
-                <div className="navbarDeployable">
-                    <div className="w-full text-center cursor-pointer">Tema</div> {/* Icono de tema claro oscuro */}
-                    <div className="w-full text-center cursor-pointer">Notificaciones</div> {/* Icono activer/desactivar notis implementar con servicios de google, android y windows mas tarde */}
-                    <button className="cursor-pointer" onClick={logoutUser}>Cerrar Sesión</button> {/* Icono de cerrar sesion */}
-                </div>
+        <div className="relative w-[22%] flex items-center justify-center">
+            <button
+                className=" h-9 w-9 cursor-pointer"
+                onClick={() => setIsOpenDepoyable(!isOpenDepoyable)}
+            >
+                <RegisterLoginUserIcon />
+            </button>
+            <div className={`navbarSideSection ${isOpenDepoyable ? "open" : ""}`}>
+                <ul className="navbarDeployable">
+                    <li className="w-full px-20 text-center cursor-pointer rounded-lg hover:bg-blue-400 flex justify-start">
+                        <button className="h-5 flex items-center gap-4">
+                            <DarkModeIcon />
+                            Tema
+                        </button>
+                    </li>
+                    <li className="w-full px-20 text-center cursor-pointer rounded-lg hover:bg-blue-400 flex justify-start">
+                        <button>
+                            <NotificationsIcon />
+                            Notificaciones
+                        </button>
+                    </li>
+                    <li className="w-full px-20 text-center cursor-pointer rounded-lg hover:bg-blue-400 flex justify-start">
+                        <button className="w-full" onClick={logoutUser}>
+                            <LogOutIcon />
+                            Cerrar Sesión
+                        </button>
+                    </li>
+                </ul>
             </div>
+        </div>
     )
 }
