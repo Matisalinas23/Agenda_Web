10c1a6c89eadaf1dbc52b0da84a368c5107fca43
Matias Salinas <matisalinas1602@gmail.com>
2026-02-18 15:45:34 -0300

    importation annesessary

--- src/components/modals/DeleteNote.tsx
+++ src/components/modals/DeleteNote.tsx
@@ -1,7 +1,6 @@
 import useDate from "../../hooks/useDate";
 import { useNotes } from "../../hooks/useNotes";
 import type { INote } from "../../interfaces/notes"; 
-import { ButtonCancelSmall } from "../Buttons/ButtonCancel";
 
 export default function DeleteNote({ note, closeModal }: { note: INote, closeModal: () => void }) {
     const { removeNote } = useNotes()
