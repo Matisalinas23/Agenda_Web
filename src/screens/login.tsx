b07b7994e3db2879911de4550f7ab353b39f01cc
Matias Salinas <matisalinas1602@gmail.com>
2026-02-25 12:47:24 -0300

    code reorganization

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -22,26 +22,30 @@ export default function Login() {
     return (
         <div className="h-full flex bg-background dark:bg-background-dark flex-col items-center justify-center gap-8">
             <form className=" text-white w-fit bg-secondary dark:bg-secondary-dark rounded-3xl p-12 flex flex-col gap-12 items-center" onSubmit={handleSubmit}>
-                    <div className="h-16 w-16">
-                        <RegisterLoginUserIcon />
-                    </div>
-                    
-                    <div className="flex flex-col gap-4">
-                        <input className="text-accent dark:text-accent-dark font-normal border-b border-blue-900/50 dark:border-blue-900/80 text-lg px-1 py-1" name="email"
-                            value={formValues.email} onChange={handleChange} type="text" placeholder="Correo"
-                        />
-                        <input className="text-accent dark:text-accent-dark font-normal border-b border-blue-900/50 dark:border-blue-900/80 text-lg px-1 py-1 mb-4" name="password"
-                            value={formValues.password} onChange={handleChange} type="password"
-                            hidden={false} placeholder="Contraseña"
-                        />
-                    </div>
+                <div className="h-16 w-16">
+                    <RegisterLoginUserIcon />
+                </div>
 
-                    <ButtonLogin type="submit" text="Iniciar sesión" /> {/* This button is a ButtonRegister component used as ButtonLogin */}
-                </form>
+                <div className="flex flex-col gap-4">
+                    <input
+                        className="text-accent dark:text-accent-dark font-normal border-b border-blue-900/50 dark:border-blue-900/80 text-lg px-1 py-1"
+                        name="email" type="text" placeholder="Correo"
+                        value={formValues.email} onChange={handleChange} 
+                    />
+                    <input className="text-accent dark:text-accent-dark font-normal border-b border-blue-900/50 dark:border-blue-900/80 text-lg px-1 py-1 mb-4"
+                        name="password" type="password" placeholder="Contraseña" hidden={false}
+                        value={formValues.password} onChange={handleChange}
+                    />
+                </div>
+
+                <ButtonLogin type="submit" text="Iniciar sesión" /> {/* This button is a ButtonRegister component used as a ButtonLogin */}
+            </form>
 
             <div className="flex gap-2 dark:text-white">
                 <p>¿Aún no tienes una cuenta?</p>
-                <Link to={"/register"} className="text-blue-700 dark:text-blue-500 hover:text-primary">Crear una cuenta</Link>
+                <Link to={"/register"} className="text-blue-700 dark:text-blue-500 hover:text-primary">
+                    Crear una cuenta
+                </Link>
             </div>
         </div>
     )
