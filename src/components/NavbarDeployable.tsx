ed153b6efb65c49dfe56eb1140a929b69097fce6
Matias Salinas <matisalinas1602@gmail.com>
2026-04-07 18:29:05 -0300

    feat(notifications): add per-note tracking and auto-reset on date edit

--- src/components/NavbarDeployable.tsx
+++ src/components/NavbarDeployable.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useAuth } from "../hooks/useAuth"
 import "./css/NavbarDeployable.css"
 import { RegisterLoginUserIcon } from "./Icons/RegisterLoginUserIcon"
@@ -6,10 +6,52 @@ import { DarkModeIcon } from "./Icons/DarkModeIcon"
 import { NotificationsIcon } from "./Icons/NotificationsIcon"
 import { LogOutIcon } from "./Icons/LogOutIcon"
 import { toggleTheme } from "../utils/theme"
+import { NotificationsOffIcon } from "./Icons/NotificationsOffIcon"
+import { ButtonAcceptSmall } from "./Buttons/ButtonAccept"
+import { ButtonCancelSmall } from "./Buttons/ButtonCancel"
+
+const ModalToggleNotifications = ({ setIsModalNotifications, isNotifications, setIsNotifications }) => {
+    return (
+        <div className=" bg-secondary-dark max-w-100 h-fit rounded-2xl p-8 flex flex-col items-center justify-center gap-12">
+            <p className="text-xl text-center">{isNotifications === "true"
+                ? "¿ Estas seguro que deseas desactivar las notificaciones ?"
+                : "¿ Estas seguro que deseas activar las notificaciones ?"
+            }</p>
+            <div className="w-full flex justify-between">
+                <ButtonCancelSmall
+                    onClick={() => {
+                        setIsModalNotifications(false)
+                    }}
+                    text="No"
+                />
+                <ButtonAcceptSmall
+                    onClick={() => {
+                        const isNotis = isNotifications === "true" ? "false" : "true"
+                        localStorage.setItem("notifications", isNotis)
+                        setIsNotifications(isNotis)
+                        setIsModalNotifications(false)
+                    }}
+                    type="button"
+                    text="Si"
+                />
+            </div>
+        </div>
+    )
+}
 
 export default function NavbarDeployable() {
     const { logoutUser } = useAuth()
     const [isOpenDepoyable, setIsOpenDepoyable] = useState<boolean>(false)
+    const [isNotification, setIsNotifications] = useState<"true" | "false">("false")
+    const [isModalNotifications, setIsModalNotifications] = useState<boolean>(false)
+
+    useEffect(() => {
+        const storedNotifications = localStorage.getItem("notifications") as "true" | "false"
+        if (storedNotifications) {
+            setIsNotifications(storedNotifications)
+        }
+    }, [])
+
     return (
         <div className="relative w-[16%] flex items-center justify-center">
             <button
@@ -30,9 +72,17 @@ export default function NavbarDeployable() {
                         </button>
                     </li>
                     <li className="min-w-38 px-2 flex justify-start rounded-xl hover:bg-blue-400">
-                        <button className="h-6 cursor-pointer flex items-center gap-4">
-                            <NotificationsIcon />
-                            Notificaciones
+                        <button className="h-6 cursor-pointer" onClick={() => { setIsModalNotifications(true) }}>
+                            {isNotification === "true"
+                                ? <div className="flex items-center gap-4">
+                                    <NotificationsOffIcon />
+                                    <p>Desactiavar</p>
+                                </div>
+                                : <div className="flex items-center gap-4">
+                                    <NotificationsIcon />
+                                    <p>Activar</p>
+                                </div>
+                            }
                         </button>
                     </li>
                     <li className="min-w-38 px-2 flex justify-start rounded-xl hover:bg-blue-400">
@@ -43,6 +93,16 @@ export default function NavbarDeployable() {
                     </li>
                 </ul>
             </div>
+
+            {isModalNotifications &&
+                <div className="fixed inset-0 top-1/2 left-1/2 -translate-1/2 z-100 w-full h-full bg-black/50 flex justify-center items-center">
+                    <ModalToggleNotifications
+                        isNotifications={isNotification}
+                        setIsModalNotifications={setIsModalNotifications}
+                        setIsNotifications={setIsNotifications}
+                    />
+                </div>
+            }
         </div>
     )
 }
