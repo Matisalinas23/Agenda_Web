00f75437879453825636e4b6dc5a87774f16c030
Matias Salinas <matisalinas1602@gmail.com>
2026-04-08 17:09:59 -0300

    fix(auth): make PrivateRoute reactive to Zustand store

--- src/components/layouts/PrivateRoutes.tsx
+++ src/components/layouts/PrivateRoutes.tsx
@@ -1,7 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
+import useAuthStore from "../../store/useAuthStore";
 
 export function PrivateRoute() {
-  const token = localStorage.getItem("token")
+  const { token } = useAuthStore(state => state);
 
   if (!token) {
     return <Navigate to="/login" replace />;
